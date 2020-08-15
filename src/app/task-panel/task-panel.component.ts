6146b7608839f711c53e78fc9d6c4032a5e5645e
kemora08 <kevin_mora89@hotmail.com>
2020-08-14 21:21:42 -0500

    add task-panel component

--- src/app/task-panel/task-panel.component.ts
+++ src/app/task-panel/task-panel.component.ts
@@ -2,14 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-task',
-  templateUrl: './task.component.html',
-  styleUrls: ['./task.component.css']
+  templateUrl: './task-panel.component.html',
+  styleUrls: ['./task-panel.component.css']
 })
 export class TaskComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
